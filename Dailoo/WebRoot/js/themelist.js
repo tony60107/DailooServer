9e40f36457e29415b8997b79de47184a5a478fa0
tony60107 <tony60107@yahoo.com.tw>
2017-10-07 17:32:05 +0800

    js存放位置整理

---
+++ Dailoo/WebRoot/js/themelist.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Waiting on 2017/10/7.
+ */
+//取得地區ID
+var regionId = location.href.split("id=")[1];
+
+//如果未提供地區ID，則跳轉到鹿野地區主題列表
+if(regionId == undefined){location.href = "/themelist.html?id=e3cfc0f0-a9f5-439b-a534-efff46ced2ce"};
+
+
+window.onload = function () {
+    //更換Footer樣式
+    if (regionId == "61ae4ab6-ed11-4131-85e2-9f2be7651e65") { //如果是金峰鄉地區
+        $$("footerIframe").contentWindow.changeCss("orange");
+    }
+}
+
+//根據地區ID取得主題資料，並新增到主題列表中
+$.ajax({
+    url: "/ThemeServlet", context: document.body,
+    type: "POST",
+    data: {"method": "getThemesByRegionId", "regionId": regionId},
+    success: function (data) {
+        var themes = eval("(" + data + ")");
+        console.dir(data);
+        $$("themelist").innerHTML = "";
+        for (var i = 0; i < themes.length; i++) {
+            var title = themes[i].name.split(",");
+            var dom = '<a href="viewlist.html?id=' + themes[i].id + '">' +
+                '<div class="theme">' +
+                '<div class="cover">' +
+                '<div class="title">' + title[0] + '</div>' +
+                '<div class="title-eng">' + (title[1] == undefined ? "" : title[1]) + '</div>' +
+                '</div>' +
+                '<img alt="" src="/ResourceServlet?url=' + themes[i].behalfPhotoUrl + '"/>' +
+                '</div>' +
+                '</a>';
+            $$("themelist").innerHTML = $$("themelist").innerHTML + dom;
+        }
+    },
+});
+
+//取得地區名稱
+$.ajax({
+    url: "/RegionServlet", context: document.body,
+    type: "POST",
+    data: {"method": "getRegionById", "id": regionId},
+    success: function (data) {
+        var region = eval("(" + data + ")");
+        $$("title").innerHTML = region.name + "周邊導覽";
+    },
+});
+
+history.replaceState(null, null, location.href);
---
+++ Dailoo/WebRoot/js/viewlist.js
@@ -0,0 +1,103 @@
+/**
+ * Created by Waiting on 2017/10/7.
+ */
+//獲取主題ID
+var themeId = location.href.split("id=")[1];
+//如果沒有主題ID，則跳轉到鹿野地區主題列表
+if(typeof themeId == 'undefined'){location.href = "/themelist.html?id=e3cfc0f0-a9f5-439b-a534-efff46ced2ce"}
+
+var themelist = $$("themelist");
+var title = $$("title");
+
+//依據主題ID，從伺服器中獲取該主題下的景點
+getViewpointsDataFromServer(themeId);
+
+//獲取主題列表並新增到導航欄中
+$.ajax({
+    url: "/ThemeServlet", context: document.body,
+    type: "POST",
+    data: {"method": "getThemesByThemeId", "id": themeId},
+    success: function (data) {
+        var themes = eval("(" + data + ")");
+        console.dir(themes);
+        for(var i = 0; i < themes.length; i++){
+            var dom = '<div class="theme" onclick="getViewpointsDataFromServer(\''+ themes[i].id +'\',\'' + themes[i].name.split(",")[0] + '\', this);">' + themes[i].name.split(",")[0] + '</div>';
+            themelist.innerHTML = themelist.innerHTML + dom;
+            if(themes[i].id == themeId) {
+                title.innerHTML = themes[i].name.split(",")[0] + '<span id="drop" class="drop"></span>';
+                $$("backward").href = "/themelist.html?id=" + themes[i].regionId;
+            }
+        }
+        //增加返回按鈕
+        themelist.innerHTML = themelist.innerHTML +  '<div class="reply" onclick="$$(\'drop\').style.display = \'inline-block\'; themelist.style.display = \'none\';"><img src="images/viewlist/reply.png"/></div>';
+    },
+});
+
+history.replaceState(null, null, location.href);
+
+//依據主題ID，從伺服器中獲取該主題下的景點
+function getViewpointsDataFromServer(themeId, themeName, btn) {
+
+    //更換標題文字
+    if(themeName != null) title.innerHTML = themeName + '<span id="drop" class="drop"></span>';
+    $.ajax({
+        url: "/ViewpointServlet", context: btn,
+        type: "POST",
+        data: {"method": "getViewpointSimplesByThemeIdAndPublish", "themeId": themeId},
+        success: function (data) {
+            var vps = eval("(" + data + ")");
+            console.dir(vps);
+            initDataFromServer(vps);
+        },
+    });
+}
+
+//將該主題的景點資料，新增到景點列表中
+function initDataFromServer(vps){
+    themelist.style.display = "none";
+    $$("viewlist").innerHTML = "";
+    for(var i = 0; i < vps.length; i++) {
+        var dom = '<a class="view" href="viewpoint.html?utm_source=InSite&utm_campaign='+ vps[i].name + '_' + vps[i].subtitle + '&id=' + vps[i].id +'">' +
+            '<img src="/ResourceServlet?url=' + vps[i].behalfPhotoUrl + '">' +
+            '<div class="cover"></div>' +
+            '<div class="title">' + vps[i].name + '</div>' +
+            '<img class="speaker-photo fl" src="/ResourceServlet?url='+ vps[i].speakerPhotoUrl +'" alt="">' +
+            '<div class="fl">' +
+            '<div class="speaker">' + vps[i].speakerName + '</div>' +
+            '<div class="time">' + parseInt(vps[i].audioLength / 60) + '分' + vps[i].audioLength % 60 + '秒</div>' +
+            '</div>' +
+            '</a>';
+
+        $$("viewlist").innerHTML = $$("viewlist").innerHTML + dom;
+    }
+}
+
+function switchDrop(){
+    var drop = $$('drop');
+    if(themelist.style.display == 'none') { //如果主題下拉選單未顯示
+        //顯示下拉選單
+        drop.style.display = 'none';
+        themelist.style.display = 'block';
+
+        //設定當前選中主題
+        var themes = $(".theme");
+        for(var i = 0; i < themes.length; i++) {
+            themes[i].className = "theme";
+            if(themes[i].innerText == title.innerText){
+                themes[i].className = "theme select";
+            }
+        }
+    } else {
+        drop.style.display = 'inline-block';
+        themelist.style.display = 'none';
+    }
+}
+
+
+window.onload = function() {
+    //更換Footer樣式
+    var jinfeng = "03326ff3-cad4-42bc-a8aa-35fca64eb2ef,8daa252d-42e6-4535-a0b6-d794d7e5029d,e6862f47-a7a3-4b22-9647-763425705f0a,10c09cb8-355c-4db9-a852-fc3d20eca556,9de8cafd-203e-4f5e-8faf-aeda29264952,4652c369-be78-460d-90e3-e0e66267069f";
+    if(jinfeng.indexOf(themeId) != -1){ //如果是金峰鄉主題
+        $$("footerIframe").contentWindow.changeCss("orange");
+    }
+}
