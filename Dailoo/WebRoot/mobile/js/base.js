aff7afa4dcdc23057e7c6697b69d565cf9d3f1f9
tony60107 <tony60107@yahoo.com.tw>
2018-05-23 14:40:10 +0800

    編輯景點功能

--- Dailoo/WebRoot/mobile/js/base.js
+++ Dailoo/WebRoot/mobile/js/base.js
@@ -90,12 +90,12 @@ function initPopMenu() {
 function previewUploadImg(event, id, callback){
     var input = event.target; //取得上傳檔案
     var reader = new FileReader(); //建立FileReader物件
-
     reader.readAsDataURL(input.files[0]); //以.readAsDataURL將上傳檔案轉換為base64字串
 
     reader.onload = function(){ //FileReader取得上傳檔案後執行以下內容
         var dataURL = reader.result; //設定變數dataURL為上傳圖檔的base64字串
         $('#' + id).attr('src', dataURL).show(); //將img的src設定為dataURL並顯示
+        console.dir($('#' + id).get(0));
     };
 
     //呼叫回調函數
