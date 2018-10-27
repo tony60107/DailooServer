b75f5e172a48f3003b497ca66df5ed086173fc03
tony60107 <tony60107@yahoo.com.tw>
2018-10-28 00:10:02 +0800

    台東縣主題地圖更新

--- Dailoo/WebRoot/js/region.js
+++ Dailoo/WebRoot/js/region.js
@@ -10,12 +10,14 @@ window.onload = function () {
         $("#footer").html(data);
         if (regionId == "61ae4ab6-ed11-4131-85e2-9f2be7651e65") { //如果是金峰鄉地區
             changeCss("jinfeng");
-        } else if(regionId == "fa179ec9-a754-45a2-8dc2-96b837594a6d"){ //如果是台東觀光'主題
+        } else if(regionId == "fa179ec9-a754-45a2-8dc2-96b837594a6d"){ //如果是台東觀光地區
             changeCss("chengkung");
-        } else if(regionId == "e031f31d-7f41-4380-b1fc-ed156a0bd96e"){ //如果是台東市主題
+        } else if(regionId == "e031f31d-7f41-4380-b1fc-ed156a0bd96e"){ //如果是台東市地區
             changeCss("taitong");
-        } else if(regionId == "0371ba44-8bf4-4135-9ac2-c7aea7717ed5"){ //如果是安平區主題
+        } else if(regionId == "0371ba44-8bf4-4135-9ac2-c7aea7717ed5"){ //如果是安平區地區
             changeCss("anping");
+        } else if(regionId == "16a0bff7-7238-4f48-b873-71e9dcee4ff0"){ //如果是台東縣地區
+            changeCss("taitongcounty");
         }
     });
 }
