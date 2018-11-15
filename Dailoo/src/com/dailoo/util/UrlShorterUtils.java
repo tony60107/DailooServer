57126a01c6336270df80b9dfde8175c1a2bd7de2
tony60107 <tony60107@yahoo.com.tw>
2018-11-15 11:10:56 +0800

    更新短網址演算法

--- Dailoo/src/com/dailoo/util/UrlShorterUtils.java
+++ Dailoo/src/com/dailoo/util/UrlShorterUtils.java
@@ -2,6 +2,7 @@ package com.dailoo.util;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.security.MessageDigest;
 import java.util.HashMap;
 
 import org.apache.http.HttpResponse;
@@ -89,26 +90,59 @@ public class UrlShorterUtils {
 				"8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
 				"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
 				"W", "X", "Y", "Z" };
-		String hex = MD5Utils.md5(key + url);// 对传入网址和混合KEY进行MD5加密
-		String[] resUrl = new String[4];
-
-		for (int i = 0; i < 4; i++) {
-			// 把加密字符按照8位一组16进制与0x3FFFFFFF进行位与运算
-			String sTempSubString = hex.substring(i * 8, i * 8 + 8);
-			long lHexLong = 0x3FFFFFFF & Long.parseLong(sTempSubString, 16);
-			String outChars = "";
-			for (int j = 0; j < 6; j++) {
-				// 把得到的值与 0x0000003D 进行位与运算，取得字符数组 chars 索引
-				long index = 0x0000003D & lHexLong;
-				// 把取得的字符相加
-				outChars += chars[(int) index];
-				// 每次循环按位右移 5 位
-				lHexLong = lHexLong >> 5;
-			}
-			resUrl[i] = outChars;
-		}
-
-		return resUrl;
+		String hex = MD5Encode(key + url); 
+	    int hexLen = hex.length(); 
+	    int subHexLen = hexLen / 8; 
+	    String[] ShortStr = new String[4]; 
+	     
+	    for (int i = 0; i < subHexLen; i++) { 
+	        String outChars = ""; 
+	        int j = i + 1; 
+	        String subHex = hex.substring(i * 8, j * 8); 
+	        long idx = Long.valueOf("3FFFFFFF", 16) & Long.valueOf(subHex, 16); 
+	         
+	        for (int k = 0; k < 6; k++) { 
+	            int index = (int) (Long.valueOf("0000003D", 16) & idx); 
+	            outChars += chars[index]; 
+	            idx = idx >> 5; 
+	        } 
+	        ShortStr[i] = outChars; 
+	    } 
+	     
+	    return ShortStr; 
 	}
+	
+	public static String byteArrayToHexString(byte[] b){
+		StringBuffer resultSb = new StringBuffer();
+		for (int i = 0; i < b.length; i++){
+			resultSb.append(byteToHexString(b[i]));
+		}
+		return resultSb.toString();
+	}
+	
+	private final static String[] hexDigits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+	
+	private static String byteToHexString(byte b){
+		int n = b;
+		if (n < 0)
+			n = 256 + n;
+		int d1 = n / 16;
+		int d2 = n % 16;
+		return hexDigits[d1] + hexDigits[d2];
+	}
+
+	public static String MD5Encode(String origin){
+		String resultString = null;
+			try {
+				
+				resultString=new String(origin);
+				MessageDigest md = MessageDigest.getInstance("MD5");
+				
+				resultString.trim();
+
+				resultString=byteArrayToHexString(md.digest(resultString.getBytes("UTF-8")));
+			}catch (Exception ex){}
+			return resultString;
+		}
 
 }
